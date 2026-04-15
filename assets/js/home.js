2e4ce20550e4c3590e61f2b67b4049ff53656d41
guyjuniorcalvet <gjcalvet@etu.uqac.ca>
2026-04-15 02:36:16 -0400

    Remove unused home.js and home.html layout

--- assets/js/home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(function () {
-    document.body.classList.add('fade-out');
-    setTimeout(function () {
-      var baseUrl = document.querySelector('meta[name="baseurl"]').getAttribute('content');
-      window.location.href = baseUrl + '/dataviz';
-    }, 1000);
-  }, 4000);
-});
